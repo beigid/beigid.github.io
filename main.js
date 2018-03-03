f7d85b505b1641bcf1b825fc42a60f980a20065a
beigid <beigid@seattleu.edu>
2018-03-03 05:10:56 -0500

    Fixed up syntax errors with the data-spy scroll attribute

--- main.js
+++ main.js
@@ -5,42 +5,25 @@
 
 
 
-
-
-
-
-
-
-
-
-
-
-// // window.addEventListener("scroll", function() {
-// //     if (window.scrollY > 100) {
-// //         $('.navbar').fadeIn();
-// //     }
-// //     else {
-// //         $('.navbar').fadeOut();
-// //     }
-// // },false);
-
 (function ($) {
   $(document).ready(function(){
     
 	// hide .navbar first
 	$(".navbar").hide();
 	
-	//fade in .navbar
+	// fade in .navbar
 	$(function () {
 		$(window).scroll(function () {
             // set distance user needs to scroll before we fadeIn navbar
-			if ($(this).scrollTop()) {
+			if ($(this).scrollTop() > 100) {
 				$('.navbar').fadeIn();
 			} else {
 				$('.navbar').fadeOut();
 			}
 		});
+
+	
 	});
+
 });
   }(jQuery));
-
