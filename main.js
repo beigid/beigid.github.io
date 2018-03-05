c89f1b7761229e289d7b6ff27c8060afa3837ddc
beigid <beigid@seattleu.edu>
2018-03-05 06:28:19 -0500

    Added more details with JavaScript and image changes

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ profileColor.addEventListener("mouseleave", function (){
 	$(function () {
 		$(window).scroll(function () {
             // set distance user needs to scroll before we fadeIn navbar
-			if ($(this).scrollTop() > 100) {
+			if ($(this).scrollTop() > 200) {
 				$('.navbar').fadeIn();
 			} else {
 				$('.navbar').fadeOut();
