830d11119ab5a8bf4790fa5cb4453013422d1e81
beigid <beigid@seattleu.edu>
2018-03-07 05:25:11 -0500

    Adjusted scroll to slide to navigation

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+
+//Code created to chnage profile image circle color when mouse hovers over
 var profileColor = document.querySelector(".imgborder");
 
 profileColor.addEventListener("mouseover", function (){
@@ -7,6 +9,7 @@ profileColor.addEventListener("mouseover", function (){
 profileColor.addEventListener("mouseleave", function (){
 	profileColor.setAttribute("style", "border: 7px solid white;")
 });
+
 
 
 
@@ -33,3 +36,34 @@ profileColor.addEventListener("mouseleave", function (){
 
 });
   }(jQuery));
+
+
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
+
+//Code made to adjust page scroll when using internal anchors
+// var shiftWindow = function() { scrollBy(0, -50) };
+// if (location.hash) shiftWindow();
+// window.addEventListener("hashchange", shiftWindow);
