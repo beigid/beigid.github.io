5d68825ca3092644d1826748a14b5fae4e37bda2
beigid <beigid@seattleu.edu>
2018-03-18 04:10:59 -0400

    updated website panels

--- main.js
+++ main.js
@@ -1,12 +1,43 @@
-// window.onLoad = function() {
-	
-	
-	
-	
-	
-	
-	
-// };
+
+
+// declare globals to hold all the links and all the panel elements
+var tabLinks;
+var tabPanels;
+
+
+		window.onload = function() {
+    // when the page loads, grab the li elements
+    tabLinks = document.getElementById("tabs").getElementsByTagName("li");
+    // Now get all the tab panel container divs
+    tabPanels = document.getElementById("containers").getElementsByTagName("div");
+
+    // activate the _first_ one
+    displayPanel(tabLinks[0]);
+
+    // attach event listener to links using onclick and onfocus, fire the displayPanel function, return false to disable the link
+    for (var i = 0; i < tabLinks.length; i++) {
+        tabLinks[i].addEventListener("click",function() {
+            displayPanel(this);
+            return false;
+        })
+    }
+}
+
+function displayPanel(tabToActivate) {
+    for (var i = 0; i < tabLinks.length; i++) {
+        if (tabLinks[i] == tabToActivate) {
+            // if it's the one to activate, change its class
+            tabLinks[i].classList.add("active");
+            // and display the corresponding panel
+            tabPanels[i].style.display = "block";
+        } else {
+            // remove the active class on the link
+            tabLinks[i].classList.remove("active");
+            // hide the panel
+            tabPanels[i].style.display = "none";
+        }
+    }
+}
 
 
 
@@ -88,7 +119,7 @@ $(document).ready(function(){
         // Add hash (#) to URL when done scrolling (default click behavior)
         window.location.hash = hash;
       });
-    } // End if
+    } 
   });
 });
 
